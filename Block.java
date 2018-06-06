160cf0064b1502aa4db120421b24f60da967ffc0
Cameron Beneteau <cameron.bellewood@gmail.com>
2018-06-06 11:14:57 -0400

    Add files via upload

--- Block.java
+++ Block.java
@@ -1,17 +1,37 @@
-//Block
-public class Block{
-	int x, y, sy;
-	public Block(int x, int y, int sy){
-		this.x = x;
-		this.y = y;
-		this.sy = sy;
+/**
+ * @(#)Ground.java
+ *
+ *
+ * @author 
+ * @version 1.00 2018/6/6
+ */
+
+import java.awt.*;
+
+class Block{
+	private String type;
+	private int x, y;
+	private Rectangle rect;
+	
+	public Block(int xx, int yy, String t){
+		x = xx;
+		y = yy;
+		type = t;
+		rect = new Rectangle(x, y, 50, 50);
 	}
-	public int getSY(){
-		return sy;
+	
+	public Rectangle getRect(){
+		return rect;
 	}
+	
+	public String getType(){
+		return type;
+	}
+	
 	public int getX(){
 		return x;
 	}
+	
 	public int getY(){
 		return y;
 	}
