2376c102641ab64350dba56380b5ab5a5faef994
jackli10345 <35533992+jackli10345@users.noreply.github.com>
2018-06-11 10:27:42 -0400

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,63 +1,231 @@
-//Player
-import java.util.*;
-class Player {
-	private int x = 950, y = 800, px = 950, py = 800, direction, minDamage, maxDamage;
-	private String type = "archer";
-	private boolean hit = false;
-	private int hp = 100;
-	public Player(){}
-	public int getX(){
-		return x;
+/**
+ * @(#)Player.java
+ *
+ *
+ * @author 
+ * @version 1.00 2018/6/6
+ */
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+
+public class Player {
+	private int maxDamage, minDamage;
+	private double x, xPos, yPos, vx, vy, sx, sy;
+	private String direction, type = "archer";
+	private boolean jump, ground;
+	int screenX = 1900;
+	int screenY = 1000;
+	private int platform, health;
+	private BufferedImage image = null;
+	
+	public Player(){
+		x = 0; //position of background image on screen
+		xPos = 100; //player X
+		yPos = 400; //player Y
+		vx = 0;
+		vy = 0;
+		direction = "right";
+		jump = false;
+		loadImage();
+		ground = true;
+		health = 100;
+	
 	}
-	public int getY(){
-		return y;
-	}
-	public int getPX(){
-		return px;
-	}
-	public int getPY(){
-		return py;
-	}
-	public int getDirection(){
-		return direction;
-	}
-	public void setClass(String t){
-		type = t;
-	}
-	public String getType(){
-		return type;
-	}
-	public int getCooldown(){
-		if(type.equals("warrior")){
-			return 50;
+	///////////////////////////////LOAD IMAGE//////////////////////////////////////
+	public void loadImage(){
+		try {
+    		image = ImageIO.read(new File("map1Mask.png"));
+		} 
+		catch (IOException e) {
 		}
-		if(type.equals("assassin")){
-			return 20;
-		}
-		if(type.equals("archer")){
-			return 40;
-		}
-		return 100;
-	}
-	public int getRange(){
-		if(type.equals("warrior")){
-			return 300;
-		}
-		if(type.equals("assassin")){
-			return 200;
-		}
-		return 100;
-	}
-	public void heal(int n){
-		if(hp + n > 100){
-			hp = 100;
+		platform = getPixelCol(image, 25, 25); //platform colour
+    }
+    ///////////////////////////////PIXEL COLOUR////////////////////////////////////
+    public int getPixelCol(BufferedImage image, int xx, int yy){
+    	return image.getRGB(xx + (int)x, yy);
+    }
+    
+    public boolean getColTop(BufferedImage image, int xx, int yy){
+    	boolean b = false;
+    	int c = image.getRGB(xx + (int) x, yy);
+    	for (int d = 0; d < 50; d++){
+    		c = image.getRGB(d + xx + (int) x, yy);
+    		if (c == platform){
+    			b = true;
+    		}
+    	}
+    	return b;
+    }
+    
+    public boolean getColBottom(BufferedImage image, int xx, int yy){
+    	boolean b = false;
+    	int c = image.getRGB(xx + (int )x, yy + 100);
+    	for (int d = 0; d < 50; d++){
+    		c = image.getRGB(d + xx + (int) x, yy + 100);
+    		if (c == platform){
+    			b = true;
+    		}
+    	}
+    	return b;
+    }
+    
+    public boolean getColRight(BufferedImage image, int xx, int yy){
+    	boolean b = false;
+    	int c = image.getRGB(xx + (int) x + 50, yy + 100);
+    	for (int d = 0; d < 100; d++){
+    		c = image.getRGB(xx + (int) x + 50, yy + d);
+    		if (c == platform){
+    			b = true;
+    		}
+    	}
+    	return b;
+    }
+    
+    public boolean getColLeft(BufferedImage image, int xx, int yy){
+    	boolean b = false;
+    	int c = image.getRGB(xx + (int) x - 1, yy + 100);
+    	for (int d = 0; d < 100; d++){
+    		c = image.getRGB(xx + (int) x - 1, yy + d);
+    		if (c == platform){
+    			b = true;
+    		}
+    	}
+    	return b;
+    }
+	///////////////////////////////VERTICAL MOVEMENT///////////////////////////////
+	public void jump(){
+		if (vy < 0){
+			for (int i = 0; i < (int) vy * -1; i++){
+				if (getColTop(image, (int) xPos, (int) yPos) == false){
+					yPos -= 1;
+					ground  = false;
+				}
+				else{
+					vy = 0;
+					ground = false;
+				}
+			}
 		}
 		else{
-			hp += n;
+			for (int i = 0; i < (int) vy; i++){
+				if (getColBottom(image, (int) xPos, (int) yPos) == false){
+					yPos += 1;
+				}
+				else{
+					vy = 0;
+					jump = false;
+					ground = true;
+				}
+			}
+		}
+		vy += 1;
+	}
+	
+	public void fall(){
+		for (int i = 0; i < (int) vy; i++){
+				if (getColBottom(image, (int) xPos, (int) yPos) == false){
+					yPos += 1;
+					ground = false;
+				}
+				else{
+					vy = 0;
+					jump = false;
+					ground = true;
+				}
+			}
+			vy += 1;
+		}
+	//////////////////////////////HORIZONTAL MOVEMENT//////////////////////////////
+	public void move(String d){
+		if (vx < 5){
+			vx += 0.4;
+		}
+		if (d.equals("right")){
+			direction = "right";
+			for (int i = 0; i < (int) vx; i++){
+				if (getColRight(image, (int) xPos, (int) yPos) == false){
+					if (xPos <= 890){
+						xPos += 1;//(int) vx;
+					}
+					else if (x >= 23090){
+						if (xPos < 1830){ //max distance
+							xPos += 1;//(int) vx;
+						}
+					}
+					else{
+						x += 1;//(int) vx;
+					}
+				}
+			}
+		}
+		else{
+			direction = "left";
+			for (int i = 0; i < (int) vx; i++){
+				if (getColLeft(image, (int) xPos, (int) yPos) == false){
+					if (xPos >= 910){
+						xPos -= 1;//(int) vx;
+					}
+					else if (x <= 10){
+						if (xPos > 10){ //min distance
+							xPos -= 1;//(int) vx;
+						}
+					}
+					else{
+						x -= 1;//(int) vx;
+					}
+				}
+			}
 		}
 	}
+	
+	public void slowDown(String d){
+		if (vx > 0){
+			vx -= 0.3;
+		}
+		if (d.equals("right")){
+			direction = "right";
+			if (getColRight(image, (int) xPos + (int) vx, (int) yPos) == false){
+				if (xPos <= 890){
+					xPos += (int) vx;
+				}
+				else if (x >= 23090){
+					if (xPos < 1830){ //max distance
+						xPos += (int) vx;
+					}
+				}
+				else{
+					x += (int) vx;
+				}
+			}
+		}
+		else{
+			direction = "left";
+			if (getColLeft(image, (int) xPos - (int) vx, (int) yPos) == false){
+				if (xPos >= 910){
+					xPos -= (int) vx;
+				}
+				else if (x <= 10){
+					if (xPos > 10){ //min distance
+						xPos -= (int) vx;
+					}
+				}
+				else{
+					x -= (int) vx;
+				}
+			}
+		}
+	}
+	/////////////////////////ATTACK///////////////////////////////
 	public Arrow shoot(int damage){
-		return new Arrow(damage, direction, px, y);
+		return new Arrow(damage, direction, (int)xPos, (int)yPos);
 	}
 	
 	public int damage(){
@@ -78,18 +246,62 @@ class Player {
 		while(d < minDamage){
 			d = random.nextInt(maxDamage);
 		}
-		System.out.println(d);
 		return d;
 	}
+	////////////////////////////GETTERS AND SETTERS////////////////////////////////
+	public int getX(){
+		return (int) x;
+	}
 	
-	public void changeX(int n){
-		x += n;
+	public int getXPos(){
+		return (int) xPos;
 	}
-	public void move(int n){
-		px += n;
+	
+	public int getYPos(){
+		return (int) yPos;
 	}
-	public void setDirection(int d){
-		direction = d;
+		
+	public int getVx(){
+		return (int) vx;
+	}
+	
+	public void setVx(int vx){
+		this.vx = vx;
+	}
+	
+	public int getVy(){
+		return (int) vy;
+	}
+	
+	public void setVy(int vy){
+		this.vy = vy;
+	}
+	
+	public void addVy(double v){
+		vy += v;
+	}
+	
+	public boolean getJump(){
+		return jump;
+	}
+	
+	public void setJump(boolean b){
+		jump = b;
+	}
+	
+	public void setGround(boolean b){
+		ground = b;
+	}
+	
+	public boolean getGround(){
+		return ground;
+	}
+	
+	public int getHealth(){
+		return health;
+	}
+	///////////////////////////////////////////////////////////////////////////////
+	public void takeDamage(int d){
+		health -= d;
 	}
 }
-	
