ad8710a55c9494e34dcbe20732e2ee7db21e72bb
Cameron Beneteau <cameron.bellewood@gmail.com>
2018-06-15 13:14:36 -0400

    Add files via upload

---
+++ Actual Final/images/blue.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="blue" tilewidth="50" tileheight="50" tilecount="798" columns="38">
+ <image source="dark blue.jpg" width="1920" height="1080"/>
+</tileset>
---
+++ Actual Final/images/lava.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="lava" tilewidth="50" tileheight="50" tilecount="42" columns="14">
+ <image source="lava.png" width="702" height="175"/>
+</tileset>
